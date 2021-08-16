abc5600bdb6402faac010509f63c6d511a11a901
Sergey <s.p.sergey@ukr.net>
2021-08-16 15:57:26 +0300

    change text

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi guys, this is a test !");
+        System.out.println("Hi guys, this is a test !");
     }
 }
